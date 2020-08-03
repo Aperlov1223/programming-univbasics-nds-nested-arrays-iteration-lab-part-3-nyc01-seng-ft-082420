a9be6cfc01357d369fc3519f6b6841a043b06772
Alice Perlov <aperlov23@gmail.com>
2020-08-03 23:07:30 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def join_nested_strings(src)
     element_index = 0
     join_nested_strings = " "
     while element_index < src[row_index].count do
-      if src[row_index][element_index].class == String
+      if src[row_index][element_index].class == String.join(" ")
         join_nested_strings = src[row_index][element_index]
     end
     element_index += 1
